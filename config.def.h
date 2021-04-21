5c5ebed8ca2e6e2d8d1cb68edd99f0b77fd68acc
Dusan Ilic <idtronicsrs@gmail.com>
2021-04-21 22:42:41 +0200

    Fixed transparent border 'bug', and changed border coler of focused window

--- config.def.h
+++ config.def.h
@@ -14,6 +14,7 @@ static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
+static const char col_darkred[]     = "#990000";
 static const char col1[]            = "#ffffff";
 static const char col2[]            = "#ffffff";
 static const char col3[]            = "#ffffff";
@@ -33,7 +34,7 @@ static const char *colors[][3]      = {
 	[SchemeCol4]  = { col4,      col_gray1, col_gray2 },
 	[SchemeCol5]  = { col5,      col_gray1, col_gray2 },
 	[SchemeCol6]  = { col6,      col_gray1, col_gray2 },
-	[SchemeSel]   = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeSel]   = { col_gray4, col_cyan,  col_darkred  },
 };
 
 /* tagging */
