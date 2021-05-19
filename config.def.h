f24ed0763dfcde11022b8ab43e0dc9e0dc8816a7
Dusan Ilic <idtronicsrs@gmail.com>
2021-05-19 12:34:44 +0200

    Some config refactoring

--- config.def.h
+++ config.def.h
@@ -1,5 +1,11 @@
 /* See LICENSE file for copyright and license details. */
+
+/* includes */
 #include <X11/XF86keysym.h> /* For audio and background settings */
+
+/* constants */
+#define TERMINAL	"st"
+#define BROWSER		"brave"
 
 /* appearance */
 static const unsigned int borderpx  = 3;        /* border pixel of windows */
@@ -11,29 +17,27 @@ static const int topbar             = 1;        /* 0 means bottom bar */
 static const int focusonwheel       = 0;
 static const char *fonts[]          = { "Mononoki Nerd Font:size=13" };
 static const char dmenufont[]       = "Mononoki Nerd Font:size=13";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
-static const char col_darkred[]     = "#990000";
-static const char col_1[]			= "#282c34";
-static const char col_2[]			= "#282c34";
-static const char col_3[]			= "#d7d7d7";
-static const char col_4[]			= "#924441";
+
+/* colors */
+static const char norm_fg_col[]		= "#d7d7d7";
+static const char norm_bg_col[]		= "#282c34";
+static const char norm_border_col[]	= "#282c34";
+static const char sel_fg_col[]		= "#d7d7d7";
+static const char sel_bg_col[]		= "#924441";
+static const char sel_border_col[]	= "#924441";
 
 enum { SchemeNorm, SchemeSel }; /* color schemes */
 
 static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-	[SchemeNorm]  = { col_3, col_1, col_2 },
-	[SchemeSel]   = { col_3, col_4, col_4 },
+	/*				  fg            bg              border   */
+	[SchemeNorm]  = { norm_fg_col,	norm_bg_col,	norm_border_col },
+	[SchemeSel]   = { sel_fg_col,	sel_bg_col,		sel_border_col },
 };
 
 /* tagging */
+static const char *tags[] = { "", "", "", "", "", "", "", "", "" };
 // static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 // static const char *tags[] = { "", "", "", "", "", "", "", "", "" };
-static const char *tags[] = { "", "", "", "", "", "", "", "", "" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -68,69 +72,83 @@ static const Layout layouts[] = {
 
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
-#define CMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { TERMINAL, NULL };
 
 static Key keys[] = {
-	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_b,      togglebar,      {0} },
-	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
-	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
-	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
-	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
-	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
-	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY,                       XK_Return, zoom,           {0} },
-	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY,			            XK_q,      killclient,     {0} },
-	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
-	{ MODKEY,                       XK_u,      setlayout,      {.v = &layouts[3]} },
-	// { MODKEY,                       XK_space,  setlayout,      {0} },
-	{ MODKEY,                       XK_space,  spawn,          CMD("next_kb_layout") },
-	{ MODKEY,                       XK_w,      spawn,          CMD("brave") },
-	{ MODKEY|ShiftMask,             XK_w,      spawn,          CMD("tabbed surf -e") },
-	{ MODKEY|ShiftMask,             XK_l,      spawn,          CMD("slock") },
-	{ MODKEY,                       XK_r,      spawn,          CMD("st -e ranger") },
-	{ MODKEY|ShiftMask,             XK_q,	   spawn,	       CMD("quitdwm") },
-	{ MODKEY|ShiftMask,             XK_backslash, spawn,       CMD("dmenucpboardrun") },
-	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
-	{ MODKEY|ShiftMask,             XK_s,      togglesticky,   {0} },
-	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
-	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
-	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
-	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-	TAGKEYS(                        XK_1,                      0)
-	TAGKEYS(                        XK_2,                      1)
-	TAGKEYS(                        XK_3,                      2)
-	TAGKEYS(                        XK_4,                      3)
-	TAGKEYS(                        XK_5,                      4)
-	TAGKEYS(                        XK_6,                      5)
-	TAGKEYS(                        XK_7,                      6)
-	TAGKEYS(                        XK_8,                      7)
-	TAGKEYS(                        XK_9,                      8)
-	{ 0, XF86XK_AudioMute,          spawn,		CMD("pamixer -t; kill -37 $(pidof dwmblocks)") },
-	{ 0, XF86XK_AudioRaiseVolume,	spawn,		CMD("pamixer --allow-boost -i 5; kill -37 $(pidof dwmblocks)") },
-	{ 0, XF86XK_AudioLowerVolume,	spawn,		CMD("pamixer --allow-boost -d 5; kill -37 $(pidof dwmblocks)") },
-	{ 0, XF86XK_MonBrightnessUp,    spawn,      CMD("backlight --inc 10") },
-	{ 0, XF86XK_MonBrightnessDown,  spawn,      CMD("backlight --dec 10") },
-	{ MODKEY,                       XK_Down,   moveresize,     {.v = "0x 25y 0w 0h" } },
-	{ MODKEY,                       XK_Up,     moveresize,     {.v = "0x -25y 0w 0h" } },
-	{ MODKEY,                       XK_Right,  moveresize,     {.v = "25x 0y 0w 0h" } },
-	{ MODKEY,                       XK_Left,   moveresize,     {.v = "-25x 0y 0w 0h" } },
-	{ MODKEY|ShiftMask,             XK_Down,   moveresize,     {.v = "0x 0y 0w 25h" } },
-	{ MODKEY|ShiftMask,             XK_Up,     moveresize,     {.v = "0x 0y 0w -25h" } },
-	{ MODKEY|ShiftMask,             XK_Right,  moveresize,     {.v = "0x 0y 25w 0h" } },
-	{ MODKEY|ShiftMask,             XK_Left,   moveresize,     {.v = "0x 0y -25w 0h" } },
+	/* modifier            key                        function        argument */
+
+	/* Spawning programs */
+	{ MODKEY,              XK_p,                      spawn,          {.v = dmenucmd } },
+	{ MODKEY|ShiftMask,    XK_Return,                 spawn,          {.v = termcmd } },
+	{ MODKEY,              XK_space,                  spawn,          SHCMD("next_kb_layout") },
+	{ MODKEY,              XK_w,                      spawn,          SHCMD(BROWSER) },
+	{ MODKEY|ShiftMask,    XK_w,                      spawn,          SHCMD("tabbed surf -e") },
+	{ MODKEY|ShiftMask,    XK_l,                      spawn,          SHCMD("slock") },
+	{ MODKEY,              XK_r,                      spawn,          SHCMD(TERMINAL " -e ranger") },
+	{ MODKEY|ShiftMask,    XK_q,	                  spawn,          SHCMD("dmenu_system") },
+	{ MODKEY|ShiftMask,    XK_backslash,              spawn,          SHCMD("dmenu_clipboard_run") },
+
+	/* Layout */
+	{ MODKEY,              XK_t,                      setlayout,      {.v = &layouts[0]} },
+	{ MODKEY,              XK_f,                      setlayout,      {.v = &layouts[1]} },
+	{ MODKEY,              XK_m,                      setlayout,      {.v = &layouts[2]} },
+	{ MODKEY,              XK_u,                      setlayout,      {.v = &layouts[3]} },
+	{ MODKEY,              XK_0,                      view,           {.ui = ~0 } },
+	{ MODKEY|ShiftMask,    XK_0,                      tag,            {.ui = ~0 } },
+	{ MODKEY,              XK_j,                      focusstack,     {.i = +1 } },
+	{ MODKEY,              XK_k,                      focusstack,     {.i = -1 } },
+	{ MODKEY,              XK_i,                      incnmaster,     {.i = +1 } },
+	{ MODKEY,              XK_d,                      incnmaster,     {.i = -1 } },
+	{ MODKEY,              XK_h,                      setmfact,       {.f = -0.05} },
+	{ MODKEY,              XK_l,                      setmfact,       {.f = +0.05} },
+	{ MODKEY,              XK_Return,                 zoom,           {0} },
+	{ MODKEY,              XK_Tab,                    view,           {0} },
+	{ MODKEY,              XK_q,                      killclient,     {0} },
+	{ MODKEY|ShiftMask,    XK_space,                  togglefloating, {0} },
+	{ MODKEY|ShiftMask,    XK_s,                      togglesticky,   {0} },
+	{ MODKEY,              XK_b,                      togglebar,      {0} },
+
+	/* Focus a different monitor */
+	{ MODKEY,              XK_comma,                  focusmon,       {.i = -1 } },
+	{ MODKEY,              XK_period,                 focusmon,       {.i = +1 } },
+
+	/* Move client to a different monitor */
+	{ MODKEY|ShiftMask,    XK_comma,                  tagmon,         {.i = -1 } },
+	{ MODKEY|ShiftMask,    XK_period,                 tagmon,         {.i = +1 } },
+
+	/* Moving and resizing floating windows */
+	{ MODKEY,              XK_Down,                   moveresize,     {.v = "0x 25y 0w 0h" } },
+	{ MODKEY,              XK_Up,                     moveresize,     {.v = "0x -25y 0w 0h" } },
+	{ MODKEY,              XK_Right,                  moveresize,     {.v = "25x 0y 0w 0h" } },
+	{ MODKEY,              XK_Left,                   moveresize,     {.v = "-25x 0y 0w 0h" } },
+	{ MODKEY|ShiftMask,    XK_Down,                   moveresize,     {.v = "0x 0y 0w 25h" } },
+	{ MODKEY|ShiftMask,    XK_Up,                     moveresize,     {.v = "0x 0y 0w -25h" } },
+	{ MODKEY|ShiftMask,    XK_Right,                  moveresize,     {.v = "0x 0y 25w 0h" } },
+	{ MODKEY|ShiftMask,    XK_Left,                   moveresize,     {.v = "0x 0y -25w 0h" } },
+
+	/* Audio control */
+	{ 0,                   XF86XK_AudioMute,          spawn,          SHCMD("pamixer -t; kill -37 $(pidof dwmblocks)") },
+	{ 0,                   XF86XK_AudioRaiseVolume,	  spawn,          SHCMD("pamixer --allow-boost -i 5; kill -37 $(pidof dwmblocks)") },
+	{ 0,                   XF86XK_AudioLowerVolume,	  spawn,          SHCMD("pamixer --allow-boost -d 5; kill -37 $(pidof dwmblocks)") },
+
+	/* Brightness control */
+	{ 0,                   XF86XK_MonBrightnessUp,    spawn,          SHCMD("backlight --inc 10") },
+	{ 0,                   XF86XK_MonBrightnessDown,  spawn,          SHCMD("backlight --dec 10") },
+
+	/* Tags */
+	TAGKEYS(               XK_1,                                      0)
+	TAGKEYS(               XK_2,                                      1)
+	TAGKEYS(               XK_3,                                      2)
+	TAGKEYS(               XK_4,                                      3)
+	TAGKEYS(               XK_5,                                      4)
+	TAGKEYS(               XK_6,                                      5)
+	TAGKEYS(               XK_7,                                      6)
+	TAGKEYS(               XK_8,                                      7)
+	TAGKEYS(               XK_9,                                      8)
 };
 
 /* button definitions */
